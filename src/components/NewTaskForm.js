f1b51c30212dcc754a9507c2f7d98f57a557bbc5
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-07-12 10:19:57 -0400

    commit tree 180161e0d2289e611069c0e637775303851efd55

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+
+  const [ textInput, setTextInput ] = useState("")
+  const [ categoryInput, setCategoryInput ] = useState("Code")
+
+  const handleTextChange = (event) => setTextInput(event.target.value)
+  const handleCategoryChange = (event) => setCategoryInput(event.target.value)
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault()
+    onTaskFormSubmit({ text: textInput, category: categoryInput})
+  }
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
+    <form 
+      className="new-task-form"
+      onSubmit={handleSubmitForm}  
+      >
+    <label>
+      Details
+      <input 
+        name="text" 
+        onChange={handleTextChange}
+        type="text"
+        value={textInput}
+    />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select 
+          name="category"
+          onChange={handleCategoryChange}
+          value={categoryInput}
+      >
+        {
+          categories.map((category) => (
+            category === "All" ? null : <option value={category}>{category}</option>
+          ))
+        }
         </select>
       </label>
       <input type="submit" value="Add task" />
